cdb8172dbd97fcaeaf63dd184f5c97fafd96f75e
David Gilady <david.g@qualisystems.com>
2020-01-01 15:11:17 +0200

    Adding a spacesniffer package

---
+++ spacesniffer/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop';
+$installersFolder = (Get-QualiIntallersPathDetails).Path
+
+$packageName = 'spacesniffer'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = "$installersFolder\spacesniffer_1_3_0_2.zip"
+
+$packageArgs = @{
+    packageName   = $packageName
+    unzipLocation = $toolsDir
+    url           = $url
+    checksum      = '156C45CB53A283D0B919CA8B1E0FAF3B967C896B8A580B962E493A27C7F465C2'
+    checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
