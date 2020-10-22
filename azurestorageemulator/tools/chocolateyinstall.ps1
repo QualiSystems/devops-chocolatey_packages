32a0fb666175d585730aa5fbdbbc1803bca1769a
David Gilady <david.g@qualisystems.com>
2020-10-22 19:09:57 +0300

    azure storage emulator package

---
+++ azurestorageemulator/tools/chocolateyinstall.ps1
@@ -0,0 +1,31 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+
+$packageName = 'azurestorageemulator' # arbitrary name for the package, used in messages
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installersFolder = (Get-QualiIntallersPathDetails).Path
+$url = "$installersFolder\microsoftazurestorageemulator.msi"
+$url64 = '' # 64bit URL here or remove - if installer is both, use $url
+#$fileLocation = Join-Path $toolsDir 'NAME_OF_EMBEDDED_INSTALLER_FILE'
+#$fileLocation = Join-Path $toolsDir 'SHARE_LOCATION_OF_INSTALLER_FILE'
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'msi' #only one of these: exe, msi, msu
+  url            = $url
+  url64bit       = $url64
+  #file         = $fileLocation
+
+  #MSI
+  silentArgs     = "/qn /norestart /l*v `"$env:TEMP\chocolatey\$($packageName)\$($packageName).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  validExitCodes = @(0, 3010, 1641)
+  # optional, highly recommended
+  softwareName   = 'microsoft azure storage emulator*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  checksum       = '4ab98e9a50e9984fef497114fc5d9340aa2cef46f86ffa85fc0b5b4c4365cf53'
+  checksumType   = 'sha256' #default is md5, can also be sha1
+  checksum64     = '4ab98e9a50e9984fef497114fc5d9340aa2cef46f86ffa85fc0b5b4c4365cf53'
+  checksumType64 = 'sha256' #default is checksumType
+}
+
+Install-ChocolateyPackage @packageArgs
--- make.ps1
+++ make.ps1
@@ -3,16 +3,16 @@ param
 (
     [parameter(Position = 0, ParameterSetName = "clean", Mandatory = $true, HelpMessage = "Clean all build outputs")]
     [Switch]$Clean,
-	
+
     [parameter(Position = 0, ParameterSetName = "build", Mandatory = $true, HelpMessage = "build all packages or a specific set of packages")]
     [Switch]$Build,
-    
+
     [parameter(Position = 0, ParameterSetName = "publish", Mandatory = $true, HelpMessage = "publish all packages or a specific set of packages")]
     [Switch]$Publish,
-    
-    [parameter(Position = 0, ParameterSetName = "buildAndPublish", Mandatory = $true, HelpMessage = "clean, build and publish all packages or a specific set of packages")]	
+
+    [parameter(Position = 0, ParameterSetName = "buildAndPublish", Mandatory = $true, HelpMessage = "clean, build and publish all packages or a specific set of packages")]
     [Switch]$BuildAndPublish,
-    
+
     [parameter(Position = 1, ParameterSetName = "build", Mandatory = $false, HelpMessage = "if specified only these packages will be build")]
     [parameter(Position = 1, ParameterSetName = "buildAndPublish", Mandatory = $false, HelpMessage = "if specified only these packages will be build and published")]
     [string[]]$PackageNames = $null
@@ -26,7 +26,7 @@ $outputPath = Join-Path -Path $rootPath -ChildPath '.output'
 
 function Test-ContainPackage([string]$longPackageName) {
     foreach ($packagesName in $PackageNames) {
-        if ($longPackageName -match  $packagesName) { return $true }
+        if ($longPackageName -match $packagesName) { return $true }
     }
     return $false;
 }
@@ -40,21 +40,21 @@ function Get-PackagesDisplayName($packages) {
     if ($null -eq $PackageNames) {
         return 'all packages'
     }
-    else { 
+    else {
         return $packages | foreach { $_.BaseName }
     }
 }
 
 function Clean {
     Write-host 'Deleting output folder'
-    Remove-Item -Path $outputPath -Recurse -Force -Confirm:$false
+    Remove-Item -Path $outputPath -Recurse -Force -Confirm:$false -ErrorAction SilentlyContinue
 }
 
-function Build {    
+function Build {
     New-Item -ItemType Directory $outputPath -Force -Confirm:$false -ErrorAction SilentlyContinue | Out-Null
     $allPackages = Get-ChildItem -Recurse -Path $rootPath -Filter "*.nuspec"
     $packages = Get-FilteredPackages $allPackages
-    
+
     Write-host "Building $(Get-PackagesDisplayName $packages)"
     $packages | foreach { choco pack --outputdirectory '.output' $_.FullName }
 }
