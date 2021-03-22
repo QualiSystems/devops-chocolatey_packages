ada1faffd8bff2043a70c1b58d27f42e8ca446c9
David Gilady <david.g@qualisystems.com>
2021-03-22 14:11:02 +0200

    erlang

---
+++ erlang/tools/chocolateyInstall.ps1
@@ -0,0 +1,19 @@
+﻿$installersPath = (Get-QualiIntallersPathDetails).Path
+
+$package = 'erlang'
+$version = '21.3'
+
+$params = @{
+  PackageName    = $package
+  FileType       = 'exe'
+  SilentArgs     = '/S'
+  Url            = "$installersPath\otp_win32_$version.exe"
+  CheckSum       = 'f1e8153530ef91c9cdc4a73593fe0fe41755412244370060bd8e4b80572ba242'
+  CheckSumType   = 'sha256'
+  Url64          = "$installersPath\otp_win64_$version.exe"
+  CheckSum64     = '4BFF2C3A537351852A914F1C1FE0DE2C2DF87AF6C501C552F323ABED08123D69'
+  CheckSumType64 = 'sha256'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @params
---
+++ erlang/tools/chocolateyUninstall.ps1
@@ -0,0 +1,2 @@
+﻿$erl_version = '10.13'
+start-process -wait "C:\Program Files\erl$erl_version\uninstall.exe /S"
